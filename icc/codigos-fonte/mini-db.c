fa46e40bf2995cad2ee39d6594c2650944b5704b
Gustavo Vieira <gvieira.26gomes@gmail.com>
2025-06-30 14:13:24 -0300

    init

--- icc/codigos-fonte/mini-db.c
+++ icc/codigos-fonte/mini-db.c
@@ -204,11 +204,13 @@ void find_registry(Registry **registries, int registries_count, char *line)
     }
     else if (strcmp(search_field, "login") == 0)
     {
+        char login[16];
+        sscanf(search_value, "\"%15[^\"]", login);
         for (int i = 0; i < registries_count; i++)
         {
             if (registries[i] != NULL)
             {
-                if (strcmp(registries[i]->login, search_value) == 0)
+                if (strcmp(registries[i]->login, login) == 0)
                 {
                     print_registry(*registries[i]);
                     found = 1;
@@ -222,11 +224,13 @@ void find_registry(Registry **registries, int registries_count, char *line)
     }
     else if (strcmp(search_field, "password") == 0)
     {
+        char password[31];
+        sscanf(search_value, "\"%30[^\"]", password);
         for (int i = 0; i < registries_count; i++)
         {
             if (registries[i] != NULL)
             {
-                if (strcmp(registries[i]->password, search_value) == 0)
+                if (strcmp(registries[i]->password, password) == 0)
                 {
                     print_registry(*registries[i]);
                     found = 1;
@@ -315,11 +319,13 @@ void delete_registry(Registry ***registries, int *registries_count_pointer, char
 
     else if (strcmp(search_field, "login") == 0)
     {
+        char login[16];
+        sscanf(search_value, "\"%15[^\"]", login);
         for (int read_index = 0; read_index < *registries_count_pointer; read_index++)
         {
             if ((*registries)[read_index] != NULL)
             {
-                if (strcmp((*registries)[read_index]->login, search_value) == 0)
+                if (strcmp((*registries)[read_index]->login, login) == 0)
                 {
                     free((*registries)[read_index]);
                     (*registries)[read_index] = NULL;
@@ -335,11 +341,13 @@ void delete_registry(Registry ***registries, int *registries_count_pointer, char
     }
     else if (strcmp(search_field, "password") == 0)
     {
+        char password[31];
+        sscanf(search_value, "\"%30[^\"]", password);
         for (int read_index = 0; read_index < *registries_count_pointer; read_index++)
         {
             if ((*registries)[read_index] != NULL)
             {
-                if (strcmp((*registries)[read_index]->password, search_value) == 0)
+                if ((*registries)[read_index]->password == password)
                 {
                     free((*registries)[read_index]);
                     (*registries)[read_index] = NULL;

e47517789e82842dcbc21a33a3d3360490346359
Gustavo Vieira <gvieira.26gomes@gmail.com>
2025-06-30 14:38:04 -0300

    init

--- icc/codigos-fonte/mini-db.c
+++ icc/codigos-fonte/mini-db.c
@@ -321,7 +321,6 @@ void delete_registry(Registry ***registries, int *registries_count_pointer, char
     {
         char login[16];
         sscanf(search_value, "\"%15[^\"]\"", login);
-        printf("%s\n", login);
         for (int read_index = 0; read_index < *registries_count_pointer; read_index++)
         {
             if ((*registries)[read_index] != NULL)
@@ -344,7 +343,6 @@ void delete_registry(Registry ***registries, int *registries_count_pointer, char
     {
         char password[31];
         sscanf(search_value, "\"%30[^\"]\"", password);
-        printf("%s\n", password);
         for (int read_index = 0; read_index < *registries_count_pointer; read_index++)
         {
             if ((*registries)[read_index] != NULL)
